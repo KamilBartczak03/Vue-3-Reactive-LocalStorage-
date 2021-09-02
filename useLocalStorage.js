9f9730a1b1c68b6310cb053b5557eebcbd7f46df
KamilBartczak03 <89569524+KamilBartczak03@users.noreply.github.com>
2021-09-02 09:17:17 +0200

    Improving code readability

--- useLocalStorage.js
+++ useLocalStorage.js
@@ -3,6 +3,7 @@ import { toRefs, watch } from "vue"
 function initializeLS(name, reactive){
   const LS_EXISTS = localStorage.getItem(name)
   if(!LS_EXISTS) return localStorage.setItem(name, JSON.stringify({...reactive}))
+  
   const PARSED_LS = JSON.parse(LS_EXISTS)
   for (const key in PARSED_LS) {
     reactive[key] = PARSED_LS[key]
@@ -10,7 +11,6 @@ function initializeLS(name, reactive){
 }
 
 export default function (name, reactive) {
-
   function proxyHandler(nv, ov){
     localStorage.setItem(name, JSON.stringify(nv))
   }
